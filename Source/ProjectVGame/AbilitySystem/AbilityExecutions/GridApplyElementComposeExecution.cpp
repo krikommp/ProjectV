727aa109e87aedfb7ae3b548570c169fffcbfe06
chen yifei <113622306@qq.com>
2023-04-17 20:57:30 +0800

    【功能】元素组合基础

---
+++ Source/ProjectVGame/AbilitySystem/AbilityExecutions/GridApplyElementComposeExecution.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GridApplyElementComposeExecution.h"
+
+#include "AbilitySystemComponent.h"
+
+UGridApplyElementComposeExecution::UGridApplyElementComposeExecution()
+{
+}
+
+void UGridApplyElementComposeExecution::Execute_Implementation(
+	const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	TArray<FGameplayEffectSpecHandle> TargetEffectSpecs;
+	for (const FConditionalGameplayEffect& ConditionalEffect : ConditionalGameplayEffects)
+	{
+		if(ExecutionParams.GetTargetAbilitySystemComponent()->HasAllMatchingGameplayTags(ConditionalEffect.RequiredSourceTags))
+		{
+			FGameplayEffectSpecHandle SpecHandle = ExecutionParams.GetSourceAbilitySystemComponent()->MakeOutgoingSpec(ConditionalEffect.EffectClass, ExecutionParams.GetOwningSpec().GetLevel(), ExecutionParams.GetOwningSpec().GetContext());
+			if (SpecHandle.IsValid())
+			{
+				TargetEffectSpecs.Add(SpecHandle);
+			}
+		}
+	}
+	for(const FGameplayEffectSpecHandle& ApplyEffect : TargetEffectSpecs)
+	{
+		ExecutionParams.GetTargetAbilitySystemComponent()->ApplyGameplayEffectSpecToSelf(*ApplyEffect.Data.Get());
+	}
+}
---
+++ Source/ProjectVGame/AbilitySystem/AbilityExecutions/GridApplyElementComposeExecution.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "UObject/Object.h"
+#include "GridApplyElementComposeExecution.generated.h"
+
+/**
+ * UGridApplyElementComposeExecution
+ *
+ * 元素组合应用
+ */
+UCLASS()
+class PROJECTVGAME_API UGridApplyElementComposeExecution : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+
+public:
+	UGridApplyElementComposeExecution();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FConditionalGameplayEffect> ConditionalGameplayEffects;
+
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+};

2b5b2587c08d7f35108bad315beaab1992085306
TaoZi <1136223096@qq.com>
2023-07-24 13:20:59 +0800

    update

--- Source/ProjectVEditor/TilemapEditor/Widget/Tilemap3DEditModeWidget.cpp
+++ Source/ProjectVEditor/TilemapEditor/Widget/Tilemap3DEditModeWidget.cpp
@@ -2,7 +2,6 @@
 
 
 #include "Tilemap3DEditModeWidget.h"
-#include "Widgets/Input/SSegmentedControl.h"
 #include "SlateOptMacros.h"
 #define LOCTEXT_NAMESPACE "STilemap3DEditStatusWidget"
 BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
@@ -11,29 +10,33 @@ void STilemap3DEditModeWidget::Construct(const FArguments& InArgs, ETilemap3DEdi
 {
 	EditMode = InEditMode;
 	OnEditModeChanged = InArgs._OnEditModeChanged;
+	BaseEditMode = InArgs._BaseEditMode;
+
+	SAssignNew(CubePanelControl, SSegmentedControl<int32>)
+	.Value(EditMode) // InitialValue
+	.OnValueChanged_Lambda( [this] (int32 InValue)
+	{
+		EditMode = static_cast<ETilemap3DEditMode>(InValue);
+		// ReSharper disable once CppExpressionWithoutSideEffects
+		OnEditModeChanged.ExecuteIfBound(EditMode);
+	} ) 
+	.Value_Lambda( [this] { return static_cast<int32>(EditMode); } )  // Bound
+
+	+ SSegmentedControl<int32>::Slot(0)
+	  .Icon(FAppStyle::Get().GetBrush("LandscapeEditor.SelectComponentTool"))
+	  .Text(LOCTEXT("View", "View Mode"))
+
+	+ SSegmentedControl<int32>::Slot(1)
+	  .Icon(FAppStyle::Get().GetBrush("LandscapeEditor.AddComponentTool"))
+	  .Text(LOCTEXT("Append", "Append Cube"))
+
+	+ SSegmentedControl<int32>::Slot(2)
+	  .Icon(FAppStyle::Get().GetBrush("LandscapeEditor.DeleteComponentTool"))
+	  .Text(LOCTEXT("Remove", "Remove Cube"));
+	
 	ChildSlot
 	[
-		SNew(SSegmentedControl<int32>)
-		.Value(EditMode) // InitialValue
-		.OnValueChanged_Lambda( [this] (int32 InValue)
-		{
-			EditMode = static_cast<ETilemap3DEditMode>(InValue);
-			// ReSharper disable once CppExpressionWithoutSideEffects
-			OnEditModeChanged.ExecuteIfBound(EditMode);
-		} ) 
-		.Value_Lambda( [this] { return static_cast<int32>(EditMode); } )  // Bound
-
-		+ SSegmentedControl<int32>::Slot(0)
-		  .Icon(FAppStyle::Get().GetBrush("LandscapeEditor.SelectComponentTool"))
-		  .Text(LOCTEXT("View", "View Mode"))
-
-		+ SSegmentedControl<int32>::Slot(1)
-		  .Icon(FAppStyle::Get().GetBrush("LandscapeEditor.AddComponentTool"))
-		  .Text(LOCTEXT("Append", "Append Cube"))
-
-		+ SSegmentedControl<int32>::Slot(2)
-		  .Icon(FAppStyle::Get().GetBrush("LandscapeEditor.DeleteComponentTool"))
-		  .Text(LOCTEXT("Remove", "Remove Cube"))
+		CubePanelControl.ToSharedRef()
 	];
 }
 
--- Source/ProjectVEditor/TilemapEditor/Widget/Tilemap3DEditModeWidget.h
+++ Source/ProjectVEditor/TilemapEditor/Widget/Tilemap3DEditModeWidget.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "TilemapEditor/Tilemap3DEditorManager.h"
 #include "Widgets/SCompoundWidget.h"
+#include "Widgets/Input/SSegmentedControl.h"
 
 /**
  * 
@@ -14,16 +15,22 @@ class PROJECTVEDITOR_API STilemap3DEditModeWidget : public SCompoundWidget
 public:
 	DECLARE_DELEGATE_OneParam(FOnEditModeChanged, const ETilemap3DEditMode)
 	SLATE_BEGIN_ARGS(STilemap3DEditModeWidget)
+			: _BaseEditMode(EEM_Cube)
 		{
 		}
 
-	SLATE_EVENT(FOnEditModeChanged, OnEditModeChanged)
+		SLATE_ATTRIBUTE(ETilemap3DEditMode, BaseEditMode)
+		SLATE_EVENT(FOnEditModeChanged, OnEditModeChanged)
 	SLATE_END_ARGS()
 
 	/** Constructs this widget with InArgs */
 	void Construct(const FArguments& InArgs, ETilemap3DEditMode InEditMode);
 
 private:
-	ETilemap3DEditMode EditMode = ETilemap3DEditMode::EEM_Cube_Append;
+	ETilemap3DEditMode EditMode = EEM_Cube_Append;
+	TAttribute<ETilemap3DEditMode> BaseEditMode = EEM_Cube;
 	FOnEditModeChanged OnEditModeChanged;
+
+	TSharedPtr<SSegmentedControl<int32>> CubePanelControl;
+	TSharedPtr<SSegmentedControl<int32>> MeshPanelControl;
 };

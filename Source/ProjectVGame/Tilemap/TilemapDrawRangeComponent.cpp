34ccf97f7dd33c141168a2c0c21ec3ce16c2caae
TaoZi <1136223096@qq.com>
2023-08-04 19:10:23 +0800

    添加绘制范围的组件

---
+++ Source/ProjectVGame/Tilemap/TilemapDrawRangeComponent.cpp
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TilemapDrawRangeComponent.h"
+
+#include "GridComponents.h"
+#include "TilemapExtensionComponent.h"
+#include "Components/DecalComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+class UTilemapExtensionComponent;
+
+UTilemapDrawRangeComponent::UTilemapDrawRangeComponent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+	PrimaryComponentTick.bCanEverTick = false;	
+}
+
+void UTilemapDrawRangeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UTilemapDrawRangeComponent::DisplayPathfindingDecal(TArray<int32> Indexes)
+{
+	const APawn* Pawn = GetPawnChecked<APawn>();
+
+	const UTilemapExtensionComponent* TilemapExtensionComponent = FIND_PAWN_COMP(TilemapExtensionComponent, Pawn);
+	if (TilemapExtensionComponent == nullptr)
+		return;
+
+	for (const int32 Index : Indexes)
+	{
+		const FVector DecalLocation = TilemapExtensionComponent->GetTilemap()->GetPathfindingBlockLocation(Index);
+		UDecalComponent* DecalComponent = UGameplayStatics::SpawnDecalAtLocation(GetWorld(), TilemapExtensionComponent->GetTilemap()->GetTilemap()->MoveRangeMat, DecalSize, DecalLocation, {90.0f, 0.0f, 0.0f});
+
+		DecalComponents.Add(DecalComponent);
+	}
+}
+
+void UTilemapDrawRangeComponent::ClearAllDecalComponents()
+{
+	for (const auto& DecalComp : DecalComponents)
+	{
+		DecalComp->DestroyComponent();
+	}
+	DecalComponents.Empty();
+}
---
+++ Source/ProjectVGame/Tilemap/TilemapDrawRangeComponent.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Components/PawnComponent.h"
+#include "TilemapDrawRangeComponent.generated.h"
+
+/**
+ * UTilemapDrawRangeComponent
+ *
+ * 绘制范围组件
+ * 挂载到需要显示范围的物体上
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PROJECTVGAME_API UTilemapDrawRangeComponent : public UPawnComponent
+{
+	GENERATED_UCLASS_BODY()
+
+	UFUNCTION(BlueprintPure)
+	static UTilemapDrawRangeComponent* FindTilemapVisibleComponent(const AActor* Actor)
+	{
+		return (Actor ? Actor->FindComponentByClass<UTilemapDrawRangeComponent>() : nullptr);
+	}
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	// 绘制寻路贴画
+	UFUNCTION(BlueprintCallable)
+	void DisplayPathfindingDecal(TArray<int32> Indexes);
+
+	// 清理所有缓存的贴花
+	UFUNCTION(BlueprintCallable)
+	void ClearAllDecalComponents();
+
+private:
+	// 记录生成的贴画组件
+	UPROPERTY(Transient)
+	TArray<TObjectPtr<UDecalComponent>> DecalComponents;
+
+	// 贴画大小
+	const FVector DecalSize = { 95.0f, 45.0f, 45.0f };
+};

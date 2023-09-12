d59c6f871738c2b9d2d72259150ef9421405e25f
TaoZi <1136223096@qq.com>
2023-09-12 11:55:27 +0800

    SkillEffect窗口

---
+++ Source/ProjectVEditor/SkillEditor/EditorInterface/Window/SkillEffectWindow.cpp
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SkillEffectWindow.h"
+
+#include "SlateOptMacros.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+void SkillEffectWindow::Construct(const FArguments& InArgs)
+{
+	SEditorViewport::Construct(SEditorViewport::FArguments());
+}
+
+SkillEffectWindow::SkillEffectWindow()
+{
+}
+
+SkillEffectWindow::~SkillEffectWindow()
+{
+}
+
+TSharedRef<SEditorViewport> SkillEffectWindow::GetViewportWidget()
+{
+	return SharedThis(this);
+}
+
+TSharedPtr<FExtender> SkillEffectWindow::GetExtenders() const
+{
+	TSharedPtr<FExtender> Result(MakeShareable(new FExtender));
+	return Result;
+}
+
+void SkillEffectWindow::OnFloatingButtonClicked()
+{
+}
+
+TSharedPtr<SWidget> SkillEffectWindow::MakeViewportToolbar()
+{
+	return SNew(SCommonEditorViewportToolbarBase, SharedThis(this));
+}
+
+TSharedRef<FEditorViewportClient> SkillEffectWindow::MakeEditorViewportClient()
+{
+	PreviewScene = MakeShareable(new FPreviewScene());
+
+	TSharedPtr<FEditorViewportClient> EditorViewportClient = MakeShareable(
+		new FEditorViewportClient(nullptr, PreviewScene.Get()));
+
+	return EditorViewportClient.ToSharedRef();
+}
+
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
---
+++ Source/ProjectVEditor/SkillEditor/EditorInterface/Window/SkillEffectWindow.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SCommonEditorViewportToolbarBase.h"
+#include "SEditorViewport.h"
+#include "Widgets/SCompoundWidget.h"
+
+/**
+ * 
+ */
+class PROJECTVEDITOR_API SkillEffectWindow : public SEditorViewport, public ICommonEditorViewportToolbarInfoProvider
+{
+public:
+	SLATE_BEGIN_ARGS(SkillEffectWindow)
+		{
+		}
+
+	SLATE_END_ARGS()
+
+	void Construct(const FArguments& InArgs);
+	SkillEffectWindow();
+	virtual ~SkillEffectWindow() override;
+
+	// ICommonEditorViewportToolbarInfoProvider interface
+	virtual TSharedRef<class SEditorViewport> GetViewportWidget() override;
+	virtual TSharedPtr<FExtender> GetExtenders() const override;
+	virtual void OnFloatingButtonClicked() override;
+	// End of ICommonEditorViewportToolbarInfoProvider interface
+
+	// SEditorViewport interface
+	virtual TSharedPtr<SWidget> MakeViewportToolbar() override;
+	// End of SEditorViewport interface
+
+	protected:
+	/** SEditorViewport interface */
+	virtual TSharedRef<FEditorViewportClient> MakeEditorViewportClient() override;
+
+	TSharedPtr<FPreviewScene> PreviewScene;
+};

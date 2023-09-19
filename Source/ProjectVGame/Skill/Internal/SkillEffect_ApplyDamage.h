525f8e777e34d43bc7c5ed4cbdf05fc9c028d930
TaoZi <1136223096@qq.com>
2023-09-19 19:16:11 +0800

    update

---
+++ Source/ProjectVGame/Skill/Internal/SkillEffect_ApplyDamage.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SkillEffect_ApplyDamage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTVGAME_API USkillEffect_ApplyDamage : public UObject
+{
+	GENERATED_BODY()
+};
